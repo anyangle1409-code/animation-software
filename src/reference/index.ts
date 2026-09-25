ae84218943b2976f0e2764aec187e35386885d01
anyangle1409-code <anyangle1409@gmail.com>
2026-09-25 10:35:56 +0100

    Export internal reference engine API

---
+++ src/reference/index.ts
@@ -0,0 +1,14 @@
+export { evaluateReference } from './evaluate';
+export type { ReferenceEvaluationOptions } from './evaluate';
+export { referenceForFamily } from './library';
+export { formatReferenceReport } from './report';
+export { curlReferenceFor } from './specs/curl';
+export type {
+  ReferenceApplicability,
+  ReferenceCheckResult,
+  ReferenceCheckSpec,
+  ReferenceCheckStatus,
+  ReferenceFamilyId,
+  ReferenceReport,
+  ReferenceSpec,
+} from './types';

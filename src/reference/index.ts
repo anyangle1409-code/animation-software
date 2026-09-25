b44bc24e8e5615fc4cfdfb80e3c3dfa10b0f271d
anyangle1409-code <anyangle1409@gmail.com>
2026-09-25 10:42:27 +0100

    Export reference calibration helpers

--- src/reference/index.ts
+++ src/reference/index.ts
@@ -1,6 +1,8 @@
 export { evaluateReference } from './evaluate';
 export type { ReferenceEvaluationOptions } from './evaluate';
 export { referenceForFamily } from './library';
+export { measureBilateralRotationError, measureJointAxis, measurePhaseTiming, measureRootAxis } from './measure';
+export type { AxisMeasurement, PhaseTimingMeasurement } from './measure';
 export { formatReferenceReport } from './report';
 export { buildReviewManifest } from './reviewManifest';
 export type { ReviewCapture, ReviewManifest, ReviewMoment, ReviewMomentId } from './reviewManifest';
